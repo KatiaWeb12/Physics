f1bd74c9c2d54a4b9b19ac4aa98dd20eaabceba8
Artur Tsebaev <tsebaevartur38@gmail.com>
2024-12-22 09:22:25 +0300

    add new endpoint

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,12 +1,22 @@
+import type { AxiosResponse } from "axios";
+import { type ClassesDTO, agent } from "@/api";
 import { CardList } from "@/components";
 import "./MainPage.css";
-import formulasList from "@/constants/allFormulas";
+import { useEffect, useState } from "react";
+import { Formula } from "@/types";
 
 //Главная страница
+const REQEST_LIMIT = 10;
 export default function MainPage() {
+  const [formulasList, setFormulasList] = useState<Formula[]>([]);
+  useEffect(() => {
+    agent.get(`/all_formulas?_start=${0}&_limit=${REQEST_LIMIT}`).then(({ data }: AxiosResponse<Formula[]>) => {
+      setFormulasList(prev => prev.concat(data))
+    });
+  }, [])
   return (
     <>
-      <CardList formulas={formulasList} />
+      {Boolean(formulasList.length) && <CardList formulas={formulasList} />}
     </>
   );
 }
